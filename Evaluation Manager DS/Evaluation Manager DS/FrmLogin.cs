21f64c387f9419f2e61b13071c1582989b492773
dsabljic <domagojsabljic2510@gmail.com>
2023-05-15 08:53:37 +0200

    CS vjezba 1 - prijava korisnika

--- Evaluation Manager DS/Evaluation Manager DS/FrmLogin.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmLogin.cs
@@ -28,8 +28,10 @@ namespace Evaluation_Manager_DS {
                 MessageBoxIcon.Error);
             } else {
                 if (txtUsername.Text == username && txtPassword.Text == password) {
-                    MessageBox.Show("Dobrodošli!", "Prijavljeni ste",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FrmStudents frmStudents = new FrmStudents();
+                    Hide();
+                    frmStudents.ShowDialog();
+                    Close();
                 } else {
                     MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
