ac7ef047c8014b481916581b75b38e9cbbb58626
dsabljic <domagojsabljic2510@gmail.com>
2023-05-15 09:36:07 +0200

    Implementiran prikaz liste studenata

--- Evaluation Manager DS/Evaluation Manager DS/FrmStudents.Designer.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmStudents.Designer.cs
@@ -23,12 +23,37 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(61, 52);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 24;
+            this.dgvStudents.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvStudents.Size = new System.Drawing.Size(629, 345);
+            this.dgvStudents.TabIndex = 0;
+            // 
+            // FrmStudents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvStudents);
+            this.Name = "FrmStudents";
             this.Text = "FrmStudents";
+            this.Load += new System.EventHandler(this.FrmStudents_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudents;
     }
 }
--- Evaluation Manager DS/Evaluation Manager DS/FrmStudents.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmStudents.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Evaluation_Manager_DS.Models;
+using Evaluation_Manager_DS.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,19 @@ namespace Evaluation_Manager_DS {
     public partial class FrmStudents : Form {
         public FrmStudents() {
             InitializeComponent();
+        }
+
+        private void FrmStudents_Load(object sender, EventArgs e) {
+            ShowStudents();
+        }
+        private void ShowStudents() {
+            List<Student> students = StudentRepository.GetStudents();
+            dgvStudents.DataSource = students;
+
+            dgvStudents.Columns["Id"].DisplayIndex = 0;
+            dgvStudents.Columns["FirstName"].DisplayIndex = 1;
+            dgvStudents.Columns["LastName"].DisplayIndex = 2;
+            dgvStudents.Columns["Grade"].DisplayIndex = 3;
         }
     }
 }
