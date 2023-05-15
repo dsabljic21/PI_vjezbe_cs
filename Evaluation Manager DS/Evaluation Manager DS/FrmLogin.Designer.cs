1c8f34e8f68f6e64f4330dba2de2096518f43907
dsabljic <domagojsabljic2510@gmail.com>
2023-05-15 08:43:29 +0200

    Prijava korisnika & provjera podataka

--- Evaluation Manager DS/Evaluation Manager DS/FrmLogin.Designer.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmLogin.Designer.cs
@@ -23,19 +23,19 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtUsername.Location = new System.Drawing.Point(109, 44);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 0;
             // 
             // label1
             // 
@@ -55,34 +55,40 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Lozinka";
             // 
-            // textBox2
+            // txtPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(109, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 2;
+            this.txtPassword.Location = new System.Drawing.Point(109, 93);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
-            // button2
+            // btnLogin
             // 
-            this.button2.Location = new System.Drawing.Point(80, 145);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Prijava";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLogin.Location = new System.Drawing.Point(80, 145);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Prijava";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(246, 192);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(227, 182);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmLogin";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prijava";
             this.Load += new System.EventHandler(this.FrmLogin_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,11 +97,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- Evaluation Manager DS/Evaluation Manager DS/FrmLogin.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmLogin.cs
@@ -10,12 +10,31 @@ using System.Windows.Forms;
 
 namespace Evaluation_Manager_DS {
     public partial class FrmLogin : Form {
+        string username = "nastavnik";
+        string password = "test";
         public FrmLogin() {
             InitializeComponent();
         }
 
         private void FrmLogin_Load(object sender, EventArgs e) {
+        }
 
+        private void btnLogin_Click(object sender, EventArgs e) {
+            if (txtUsername.Text == "") {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if (txtPassword.Text == "") {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            } else {
+                if (txtUsername.Text == username && txtPassword.Text == password) {
+                    MessageBox.Show("Dobrodošli!", "Prijavljeni ste",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else {
+                    MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
