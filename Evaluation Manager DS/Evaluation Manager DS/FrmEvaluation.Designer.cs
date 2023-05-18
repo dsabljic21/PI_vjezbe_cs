f68735275973df25c2cb75e4190362c963b92c79
dsabljic <domagojsabljic2510@gmail.com>
2023-05-18 19:18:31 +0200

    Insert/Update evaluacije povezan s formom

--- Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.Designer.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.Designer.cs
@@ -74,6 +74,7 @@
             this.btnSave.TabIndex = 7;
             this.btnSave.Text = "Spremi";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
