6cd72d41a6ecc2db790616b9674052cbde360065
dsabljic <domagojsabljic2510@gmail.com>
2023-05-18 18:58:03 +0200

    Forma za evaluacije povezana s repozitorijem evaluacija

--- Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.cs
+++ Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.cs
@@ -13,6 +13,9 @@ using System.Windows.Forms;
 namespace Evaluation_Manager_DS {
     public partial class FrmEvaluation : Form {
         private Student student;
+
+        public Student SelectedStudent { get; private set; }
+
         public FrmEvaluation(Models.Student selectedStudent) {
             InitializeComponent();
             student = selectedStudent;
@@ -35,8 +38,19 @@ namespace Evaluation_Manager_DS {
            currentActivity.MaxPoints;
             txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" +
            currentActivity.MaxPoints;
-            numPoints.Minimum = 0;
-            numPoints.Maximum = currentActivity.MaxPoints;
+            // numPoints.Minimum = 0;
+            // numPoints.Maximum = currentActivity.MaxPoints;
+            var evaluation =
+ EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaluation != null) {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+                numPoints.Value = evaluation.Points;
+            } else {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
